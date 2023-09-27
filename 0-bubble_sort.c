a4b394e35a776b3407a4e324b9aaea989aae6b0f
Wiam-e <wiam.errabih@gmail.com>
2023-09-27 14:51:33 -0700

    Unknown requirement fail

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,27 @@
 #include "sort.h"
+
 /**
- * bubble_sort - sort array lements from min to max value
- * @array: array
- * @size: array size
+ * bubble_sort - sorting an array using bubble sort algorithm
+ * @array: the array to sort
+ * @size: the size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
-
-	size_t i, index, tmp = 0;
-
-	if (size < 2)
+	size_t i, j;
+	int tmp;
+	if (size < 2 || !array)
 		return;
 	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
+	{
+		for (j = 0; j < size; j++)
 		{
-			if (array[index] > array[index + 1] && array[index + 1])
+			if (array[j] < array[j - 1])
 			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
+				tmp = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = tmp;
+				print_array(array, size);
 			}
 		}
+	}
 }
