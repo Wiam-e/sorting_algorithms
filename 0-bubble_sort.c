c1464433bc5925b9123c23fc0b52be7cad0462b0
Wiam-e <wiam.errabih@gmail.com>
2023-09-27 15:12:44 -0700

    trying to fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -36,7 +36,7 @@ void bubble_sort(int *array, size_t size)
 			{
 				swap_ints(array + i, array + i + 1);
 				print_array(array, size);
-				bubbly = false;
+				bubble = false;
 			}
 		}
 		len--;
